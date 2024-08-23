c0a2e92e134e28ce861923c33207d19081a19c1c
sumit-linearloop <sumit@linearloop.io>
2024-08-23 15:06:07 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config(); // Load environment variables from .env file
+const express = require('express');
+const app = express();
+
+// Use the PORT variable from .env
+const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send(`Hello DevOps! in ${process.env.NODE_ENV}`);
+});
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} in ${process.env.NODE_ENV} mode`);
+});
